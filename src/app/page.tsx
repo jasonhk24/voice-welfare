8b7db24477c7642ca8f2c933f73c66ce46660317
jasonhk24 <jasonhk24@naver.com>
2025-05-03 21:55:05 +0900

    fix: 페이지가 PolicySearchUI를 렌더링하도록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import SpeechToChat from '../components/SpeechToChat';
+// src/app/page.tsx
+import PolicySearchUI from '../components/PolicySearchUI';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-white text-black p-6">
-      <h1 className="text-4xl font-bold mb-6">🎤 복지 정책 챗봇</h1>
-      <SpeechToChat />
-    </main>
-  );
+  return <PolicySearchUI />;
 }
